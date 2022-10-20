13a6de64cc129fa238e6943bc4240a7ed63e1f2f
Yash Kharche <reachyashk@gmail.com>
2022-10-20 19:13:49 +0530

    migrated Custom model and Signals functionality for Profile model

--- FoodOnlineMain/settings.py
+++ FoodOnlineMain/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,7 @@ DATABASES = {
         'HOST': config('HOST')
     }
 }
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
@@ -121,11 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'FoodOnlineMain/static')
 ]
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ accounts/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+#MODELS
+from .models import (
+    User,
+    UserProfile
+)
+
+# register the models here
+
+class AdminUser(UserAdmin):
+    list_display = ['email','first_name', 'last_name', 'role', 'date_joined']
+    list_display_links = ['email']
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, AdminUser)
+admin.site.register(UserProfile)
+
