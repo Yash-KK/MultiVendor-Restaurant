1a942b750791c43f1e0678b60dd6368957abd9bf
Yash Kharche <reachyashk@gmail.com>
2022-10-22 14:22:13 +0530

    Changes

--- FoodOnlineMain/settings.py
+++ FoodOnlineMain/settings.py
@@ -152,8 +152,8 @@ MESSAGE_TAGS = {
 
 
 # Email Configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yashpertisen@gmail.com'
-EMAIL_HOST_PASSWORD = 'hcwjpjyvlowtntrh'
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
